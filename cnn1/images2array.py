3ee5fb0bb432c5bb2c58a3466bf8df0576a33048
marcoruizrueda <marco.ruiz@ufba.br>
2017-07-28 12:35:34 -0300

    test

--- cnn1/images2array.py
+++ cnn1/images2array.py
@@ -4,22 +4,24 @@ import glob
 from skimage import io
 
 
-imageFolderPath = '/home/marco/Desktop/tensorflow-projects/cnn1/bears/'
+imageFolderPath = 'tensorflow-projects/cnn1/bears/'
 imagePath = glob.glob(imageFolderPath + '/*.jpeg')
 
-im_array = numpy.array()
+#im_array = []
 for img in imagePath:
 	im = io.imread(img)
 	#im = numpy.array(Image.open(img).convert('L'), 'f')
 	#print(im[:,:,0])	
 	#print("-------------------------")	
 	#print(im[:,:,0].ravel())
-	numpy.expand_dims(im_array, 0)	
-	numpy.append(im_array, im[:,:,0].ravel(),0)
+	#im_array[0] = im[:,:,0].ravel()	
+	#numpy.expand_dims(im_array, 0)	
+	#numpy.append(im_array, im[:,:,0].ravel(),0)
 	#numpy.append(im_array, im[:,:,1].ravel(),1)
 	#numpy.append(im_array, im[:,:,2].ravel(),2)
-	print(im_array.shape)	
+	im1 = im.reshape((1,im.shape[0]*im.shape[1],3))	
+	print(img)
+	print(im1.shape)	
 	break
 #im_array = numpy.array( [numpy.array(Image.open(img).convert('L'), 'f') for img in imagePath] )
-print(im_array.shape)
-print(im_array[1])
+
